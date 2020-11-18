f66775b9b01245eb520869953819a60dd9a2a161
reviakin <d.reviakin@8bridge.org>
2020-11-18 18:27:23 +0200

    create upper_case_word func

---
+++ demo.py
@@ -0,0 +1,15 @@
+def upper_case_word(word):
+    return word.upper()
+
+if __name__ == '__main__':
+
+    greetings = ["hello", 'bonjour', 'hola']
+
+    for greeting in greetings:
+        print(f"greeting: {upper_case_word(greeting)}")
+
+    name = "Dima"
+    print(name)
+
+    upper_case_name = upper_case_word(name)
+    print(f"upper case name is {upper_case_name}")
