682fde318ca11d261e61ae8da669756e7a55421b
reviakin <d.reviakin@8bridge.org>
2020-11-18 19:06:30 +0200

    add params to func

--- repos_with_most_stars.py
+++ repos_with_most_stars.py
@@ -1,14 +1,19 @@
 import requests
 
 
-def repos_with_most_stars():
+def repos_with_most_stars(total_count=10, q="stars:>50000"):
     gh_api_repo_search_url = "https://api.github.com/search/repositories"
 
-    params = {"q": "stars:>50000"}
+    params = {"q": q, "total_count": total_count}
+
     response = requests.get(gh_api_repo_search_url, params=params)
 
-    print(response.text)
+    return response
+
+
+def madeJson(req):
+    return req.json()
 
 
 if __name__ == '__main__':
-    repos_with_most_stars()
+    print(madeJson(repos_with_most_stars(total_count=5)).get("items"))
