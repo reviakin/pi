e1e3e827773223173c37d4be87057e4b4df25922
reviakin <d.reviakin@8bridge.org>
2020-12-15 00:40:55 +0200

    added example with test

---
+++ mult.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+def multiply(x, y):
+    return x * y
+
+
+# Usually tests live in a tests file, or module, not inline.
+class TestMultiply(unittest.TestCase):
+
+    def test_multiply(self):
+        test_x = 5
+        test_y = 10
+
+        self.assertEqual(multiply(test_x, test_y), 50)
+
+
+if __name__ == '__main__':
+    unittest.main()
